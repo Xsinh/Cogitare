6ee1667f216e374d7c121b34d341e172baf26d4d
Xsinh <funnybalmosh@gmail.com>
2020-09-08 21:54:53 +0300

    Redisign

--- app/src/main/java/com/implozia/cogitare/ui/MainActivity.kt
+++ app/src/main/java/com/implozia/cogitare/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.implozia.cogitare.ui
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.Observer
@@ -10,12 +11,12 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.implozia.cogitare.App
 import com.implozia.cogitare.R
 import com.implozia.cogitare.model.Note
 import com.implozia.cogitare.model.adapter.Adapter
 import kotlinx.android.synthetic.main.activity_note_details.*
+
 
 @Suppress("UNCHECKED_CAST")
 class MainActivity : AppCompatActivity() {
@@ -23,29 +24,38 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
     private lateinit var note: Note
+    private lateinit var imm: InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         note = Note()
+        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.setBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(p0: View, p1: Int) {
-
+                if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_COLLAPSED)
+                    swipeHint.visibility = View.INVISIBLE
+                else
+                    swipeHint.visibility = View.VISIBLE
             }
+
             override fun onSlide(p0: View, p1: Float) {
 
             }
         })
 
         addNote.setOnClickListener {
-            note.text = text.text.toString()
+            note.text = editTextOfNote.text.toString()
             note.done = false
             note.timestamp = System.currentTimeMillis()
             App.instance!!.noteDao.update(note)
             App.instance!!.noteDao.insert(note)
+            editTextOfNote.text.clear()
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
 
 
--- app/src/main/java/com/implozia/cogitare/ui/NoteDetailDialog.kt
+++ app/src/main/java/com/implozia/cogitare/ui/NoteDetailDialog.kt
@@ -37,11 +37,4 @@ class NoteDetailDialog(val noteText: String) : DialogFragment() {
             dialog?.window?.setLayout(width, height)
         }
     }
-//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-//        val dialog = super.onCreateDialog(savedInstanceState)
-//        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-//        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-//
-//        return dialog
-//    }
 }
